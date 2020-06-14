3c58cf7b422e99ce3cefb425375c9c3b31fec19f
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-06-14 14:29:54 -0400

    user-page

--- client/components/user-info.js
+++ client/components/user-info.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { updateEmail, updateName } from '../store/user'
+import {connect} from 'react-redux'
+import {updateEmail, updateName} from '../store/user'
 
 export class UserInfo extends React.Component {
   constructor(props) {
@@ -66,10 +66,7 @@ export class UserInfo extends React.Component {
             <div key={user.id}>
               <br />
               <h2 className="acct-details">
-                <div>Name</div>{' '}
-                <div>
-                  {user.fullName}
-                </div>{' '}
+                <div>Name</div> <div className="name">{user.fullName}</div>{' '}
                 <button
                   type="button"
                   onClick={this.toggleName}
@@ -94,7 +91,7 @@ export class UserInfo extends React.Component {
                 ) : null}
               </h2>
               <h2 className="acct-details">
-                <div>Email</div> <div>{user.email}</div>{' '}
+                <div>Email</div> <div className="name">{user.email}</div>{' '}
                 <button
                   type="button"
                   onClick={this.toggleEmail}
